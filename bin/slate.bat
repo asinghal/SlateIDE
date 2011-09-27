7d56585844b0200b81a1ce48b9ea7197a8b358bc
Aishwarya Singhal <asinghal79@gmail.com>
2011-09-27 17:41:28 +0530

    added executable scripts

--- bin/slate.bat
+++ bin/slate.bat
@@ -1,13 +1,15 @@
 
 rem Please define the Java home here
-JAVA_HOME=%JAVA_HOME%
+set JAVA_HOME="%JAVA_HOME%"
 
 rem Please define the Scala home here
-SCALA_HOME=%SCALA_HOME%
+set SCALA_HOME=%SCALA_HOME%
 
-SLATE_HOME=..
+rem assuming we are in bin directory, we need to go a level up
+cd ..
+set SLATE_HOME=.
 
-CLASSPATH=%SLATE_HOME%\conf;%SLATE_HOME%\templates;%SLATE_HOME%\lib\lucene-core-3.3.0.jar;%SLATE_HOME%\lib\scalatest-1.2-for-scala-2.8.0.RC7-SNAPSHOT.jar;%SLATE_HOME%\lib\slate-0.1.jar;%JAVA_HOME%\lib\tools.jar;%SCALA_HOME%\lib\scala-compiler.jar
+set CLASSPATH=%SLATE_HOME%\conf;%SLATE_HOME%\templates;%SLATE_HOME%\lib\lucene-core-3.3.0.jar;%SLATE_HOME%\lib\scalatest-1.2-for-scala-2.8.0.RC7-SNAPSHOT.jar;%JAVA_HOME%\lib\tools.jar;%SLATE_HOME%\lib\slate-0.1.jar;%SCALA_HOME%\lib\scala-compiler.jar
 
 
-%SCALA_HOME%\bin\scala -cp $CLASSPATH -Xmx512M net.slate.Launch 
+%SCALA_HOME%\bin\scala -cp %CLASSPATH% net.slate.Launch 
