374d6dd2c52d41713e2cbed1fc0244f04366691d
Aishwarya Singhal <asinghal79@gmail.com>
2011-09-28 13:18:52 +0530

    added documentation

--- bin/slate.bat
+++ bin/slate.bat
@@ -9,7 +9,7 @@ rem assuming we are in bin directory, we need to go a level up
 cd ..
 set SLATE_HOME=.
 
-set CLASSPATH=%SLATE_HOME%\conf;%SLATE_HOME%\templates;%SLATE_HOME%\lib\lucene-core-3.3.0.jar;%SLATE_HOME%\lib\scalatest-1.2-for-scala-2.8.0.RC7-SNAPSHOT.jar;%SLATE_HOME%\lib\slate-0.1.jar;%SCALA_HOME%\lib\scala-compiler.jar
+set CLASSPATH=%SLATE_HOME%\conf;%SLATE_HOME%\templates;%SLATE_HOME%\lib\lucene-core-3.3.0.jar;%SLATE_HOME%\lib\scalatest-1.2-for-scala-2.8.0.RC7-SNAPSHOT.jar;%JAVA_HOME%\lib\tools.jar;%SLATE_HOME%\lib\slate-0.1.jar;%SCALA_HOME%\lib\scala-compiler.jar
 
 
 %SCALA_HOME%\bin\scala -cp %CLASSPATH% net.slate.Launch 
