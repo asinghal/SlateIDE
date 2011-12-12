8e0b00fb7af8720d8d8feec31e2beabb4ca59008
Aishwarya Singhal <asinghal79@gmail.com>
2011-12-13 00:43:55 +0530

    api lookup

--- bin/slate.bat
+++ bin/slate.bat
@@ -9,7 +9,7 @@ rem assuming we are in bin directory, we need to go a level up
 cd ..
 set SLATE_HOME=.
 
-set CLASSPATH=%SLATE_HOME%\conf;%SLATE_HOME%\templates;%SLATE_HOME%\lib\lucene-core-3.3.0.jar;%SLATE_HOME%\lib\scalatest-1.2-for-scala-2.8.0.RC7-SNAPSHOT.jar;%SLATE_HOME%\lib\slate-0.1.jar;%SCALA_HOME%\lib\scala-compiler.jar;"%JAVA_HOME%\lib\tools.jar"
+set CLASSPATH=%SLATE_HOME%\conf;%SLATE_HOME%\templates;%SLATE_HOME%\lib\lucene-core-3.3.0.jar;%SLATE_HOME%\lib\gson-2.0.jar;%SLATE_HOME%\lib\scalatest-1.2-for-scala-2.8.0.RC7-SNAPSHOT.jar;%SLATE_HOME%\lib\slate-0.1.jar;%SCALA_HOME%\lib\scala-compiler.jar;"%JAVA_HOME%\lib\tools.jar"
 
 set JAVA_OPTS=-Xms512M -Xmx512M -XX:MaxPermSize=256M
 
